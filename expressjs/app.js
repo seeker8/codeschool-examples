6fe78dc27a0df3561cb6f1637348dbbcf9c6d20c
seeker8 <chuyo_gk87@yahoo.com.mx>
2015-01-11 17:42:21 -0600

    Add support for dynamic routes and user params

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,14 +1,35 @@
 var express = require('express'),
     app = express(),
-    logger = require('./logger');
+    logger = require('./logger'),
+    jsonBlocks = {
+    	'Fixed': 'Fastened securely in position',
+    	'Movable': 'Capable of being moved',
+    	'Rotating': 'Moving in a circle around its center'
+    };
 
 app.use(logger);
 
 app.use(express.static('public'));
 
 app.get('/blocks', function(request, response){
-	var blocks = ['Fixed', 'Movible', 'Rotable'];
-	response.json(blocks);
+	var blocks = ['Fixed', 'Movable', 'Rotating'],
+	    limit = request.query.limit;
+
+	//User Params
+	if(limit >= 0){
+        response.json(blocks.slice(0, limit));
+	} else {
+	    response.json(blocks);
+    }
+});
+
+app.get('/blocks/:name', function(request, response){
+    var name = request.params.name,
+        description = jsonBlocks[name];
+    if(!description){
+    	response.status(404).json("No description found for " + name);
+    }
+    response.json(description);
 });
 
 app.listen(3000, function(){
