cf6807d511a3999eea478d851faf9ee517a14e7f
seeker8 <chuyo_gk87@yahoo.com.mx>
2015-01-12 22:17:42 -0600

    Update styles references

--- expressjs/public/js/client.js
+++ expressjs/public/js/client.js
@@ -2,14 +2,16 @@ $(function(){
     $.get('/blocks', appendToList);
     
 	function appendToList(blocks){
-		var list = [], block, content;
+		var list = [], block, content, closespan;
 		$('.block-list').addClass('list-group');
         
 		for(var i in blocks){
             block = blocks[i];
             content = '<a href="/blocks/' + block + '">' + block + '</a>';
+            closespan = '<span class="glyphicon glyphicon-remove-circle"></span>';
             var li = $('<li>');
             li.addClass('list-group-item');
+            li.append(closespan);
             li.append(content);
 			list.push(li);
 		}
